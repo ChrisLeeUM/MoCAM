dd5749f22b549974d41386dea85712f26b22880a
ChrisLee <33370820+ChrisLeeUM@users.noreply.github.com>
2023-03-31 20:44:18 +0800

    Update ai_pilot_running.py

--- end_to_end_ai_control/src/ai_pilot_running.py
+++ end_to_end_ai_control/src/ai_pilot_running.py
@@ -8,8 +8,11 @@
 import cv2
 import torch
 import rospy
+import math
+import message_filters
 import numpy as np
 from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge, CvBridgeError
 from PIL import Image as PIL_Image
 
@@ -29,47 +32,58 @@ load_checkpoint(model)
 device = torch.device("cuda:0")
 model.to(device)
 
-def image_callback(msg):
+destination = {'x': -61.8, 'y': -12.1}
+
+def callback(img_msg, odom_msg):
     print("Received an image!")
     try:
+        cmd_publisher = rospy.Publisher(
+            "/carla/ego_vehicle/vehicle_control_cmd",
+            CarlaEgoVehicleControl,
+            queue_size=10)
+        command = CarlaEgoVehicleControl()
+        current_pos_x = odom_msg.pose.pose.position.x
+        current_pos_y = odom_msg.pose.pose.position.y
+        d_x = current_pos_x - destination['x']
+        d_y = current_pos_y - destination['y']
+        distance = math.sqrt((d_x**2)+(d_y**2))
+        print(distance)
+        if (distance < 2):
+            command.throttle     = 0
+            command.steer        = 0
+            # command.reverse      = reverse
+            command.brake        = 1
+            cmd_publisher.publish(command)
+            print("Finish")
+            return
         # Convert your ROS Image message to OpenCV2
-        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+        cv2_img = bridge.imgmsg_to_cv2(img_msg, "bgr8")
     except CvBridgeError as e:
         print(e)
     else:
-        try:
-            depth_msg = rospy.wait_for_message("/carla/ego_vehicle/depth_front/image", Image, timeout = 0.5)
-            depth_img = bridge.imgmsg_to_cv2(depth_msg, "32FC1")
-            depth_img_array = np.array(depth_img, dtype = np.dtype('f8'))
-            norm_res = cv2.normalize(depth_img_array, depth_img_array, 0, 255, cv2.NORM_MINMAX)
-        except ROSException as e:
-            print(e)
-        else:
-            # Change image format from cv2 to PIL
-            raw_img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-            im_pil = PIL_Image.fromarray(raw_img_rgb)
-            rgb_torch_data = dataset_transforms(im_pil)
+        # Change image format from cv2 to PIL
+        raw_img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        im_pil = PIL_Image.fromarray(raw_img_rgb)
+        rgb_torch_data = dataset_transforms(im_pil)
 
-            rgb_torch_data = rgb_torch_data.cuda()
-            rgb_torch_data = torch.unsqueeze(rgb_torch_data, 0)
-            steer = float(model(rgb_torch_data).item())
-            print(steer)
-            # Publish Commands
-            cmd_publisher = rospy.Publisher(
-                "/carla/ego_vehicle/vehicle_control_cmd",
-                CarlaEgoVehicleControl,
-                queue_size=10)
-            command = CarlaEgoVehicleControl()
-            command.throttle     = 0.3
-            command.steer        = steer
-            # command.reverse      = reverse
-            # command.brake        = brake
-            cmd_publisher.publish(command)
+        rgb_torch_data = rgb_torch_data.cuda()
+        rgb_torch_data = torch.unsqueeze(rgb_torch_data, 0)
+        steer = float(model(rgb_torch_data).item())
+        print(steer)
+        # Publish Commands
+
+        command.throttle     = 0.3
+        command.steer        = steer
+        # command.reverse      = reverse
+        # command.brake        = brake
+        cmd_publisher.publish(command)
 
 def main():
     rospy.init_node('image_listener')
-    image_topic = "/carla/ego_vehicle/rgb_front/image"
-    rospy.Subscriber(image_topic, Image, image_callback)
+    image_topic = message_filters.Subscriber('/carla/ego_vehicle/rgb_front/image', Image)
+    odom_topic = message_filters.Subscriber('/carla/ego_vehicle/odometry', Odometry)
+    ts = message_filters.TimeSynchronizer([image_topic, odom_topic], 10)
+    ts.registerCallback(callback)
     rospy.spin()
 
 if __name__ == '__main__':
